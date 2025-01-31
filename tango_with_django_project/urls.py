438c982b7824c9e047c248343d7ca619d997c7a3
“Kyle <2912472s@student.gla.ac.uk>
2025-01-31 16:08:54 +0000

    Chapter 4 lol

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from rango import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -25,5 +28,5 @@ urlpatterns = [
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
