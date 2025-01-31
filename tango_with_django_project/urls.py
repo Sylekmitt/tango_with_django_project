69df47efef22d18699f6cd1632733e89f8fb36fd
“Kyle <2912472s@student.gla.ac.uk>
2025-01-31 11:45:18 +0000

    SAFE

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,8 +20,10 @@ from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', views.about, name='about' ),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
+    
 ]
 
