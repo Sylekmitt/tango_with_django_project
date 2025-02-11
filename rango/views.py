f540a41b52cccc23f75ae7da7b7fd10ae7c60814
“Kyle <2912472s@student.gla.ac.uk>
2025-02-11 11:11:07 +0000

    Chapter 7 complete (forgot to commit first time)

--- rango/views.py
+++ rango/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
+
+# Create your views here.
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!) 
-    # that will be passed to the template engine.
+    ''' Query the database for a list of ALL categories currently stored.
+    Order the categories by the number of likes in descending order.
+    Retrieve the top 5 ONLY -- or all if there are less than 5
+    Place the list in our context_dict dictionary (with boldmessage)
+    that will be passed to the template engine.'''
+    #this queries the category model to retreive the top 5 categories
+    #the use of '-likes' notes that it is in decending order 'likes' would give ascending
+    #pythons list syntax then takes a subset of these up to 5, as specified
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {}    
+
+    context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
-    response = render(request, 'rango/index.html', context=context_dict)
-    return response
-
-
-    
+    #render the response and send it back
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'boldmessage': 'This tutorial has been put together by Connor Coull.'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -34,11 +42,48 @@ def show_category(request, category_name_slug):
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
-
     except Category.DoesNotExist:
-        context_dict['category'] = None
+        context_dict['category'] = None 
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    # Checks if the request is HTTP POST
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            cat = form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    
+    if category is None:
+        return redirect('/rango/')
+    
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+            
+            else:
+                print(form.errors)
+                
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
 
 
