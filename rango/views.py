0575ef725963baefaec023340a3f4091d35a347f
“Kyle <2912472s@student.gla.ac.uk>
2025-02-12 11:48:54 +0000

    Chapter 10 passed

--- rango/views.py
+++ rango/views.py
@@ -6,32 +6,36 @@ from rango.forms import CategoryForm
 from rango.forms import PageForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    ''' Query the database for a list of ALL categories currently stored.
-    Order the categories by the number of likes in descending order.
-    Retrieve the top 5 ONLY -- or all if there are less than 5
-    Place the list in our context_dict dictionary (with boldmessage)
-    that will be passed to the template engine.'''
-    #this queries the category model to retreive the top 5 categories
-    #the use of '-likes' notes that it is in decending order 'likes' would give ascending
-    #pythons list syntax then takes a subset of these up to 5, as specified
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    #render the response and send it back
-    return render(request, 'rango/index.html', context=context_dict)
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+    #context_dict['visits'] = request.session['visits']
+    # Return response back to the user, updating any cookies that need changed.
+    
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Connor Coull.'}
+
+    #if request.session.test_cookie_worked():
+        #print("TEST COOKIE WORKED!")
+        #request.session.delete_test_cookie()
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Kyle Smith.'}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -85,5 +89,30 @@ def add_page(request, category_name_slug):
                 
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
 
 
